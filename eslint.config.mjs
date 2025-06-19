1e64171dd58873d93c196a8eebff155d7b5a1f1f
goudaren <690469069@qq.com>
2025-06-19 18:54:00 +0800

    feat(init): 初始化Next.js 14项目，配置TypeScript、Tailwind CSS、ESLint、Prettier和Headless UI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("prettier"),
+  {
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
 ];
 
 export default eslintConfig;
